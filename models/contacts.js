e41d69c02500ad0b653de87c658c2f4255f14679
Alhassan Ibrahim <alhassan.ibrahim@flexisaf.com>
2020-11-07 14:21:30 +0100

    Add contact schema

--- models/contacts.js
+++ models/contacts.js
@@ -1,2 +1,13 @@
 const mongoose = require('mongoose');
+const { Schema }  = require('mongoose');
 
+const ContactSchema = new Schema({
+    id: ObjectId,
+    fname: String,
+    lname: String,
+    phone: String
+  });
+
+  const Contact = mongoose.model('Contact', ContactSchema);
+
+  module.exports = Contact;
