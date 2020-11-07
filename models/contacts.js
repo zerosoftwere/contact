a0b83381bf0518ff325652d6f5deabe85a372f4e
Muhammad <dulex51@hotmail.com>
2020-11-07 14:19:58 +0100

    created models

--- models/contacts.js
+++ models/contacts.js
@@ -1,2 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+const contactSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+    max: 255,
+  },
+  lastname: {
+    type: String,
+    required: true,
+    max: 255,
+  },
+  phone: {
+    type: number,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Contact", contactSchema);
--- models/users.js
+++ models/users.js
@@ -1,1 +1,21 @@
-const mongoose = requrie('mongoose');
+const mongoose = requrie("mongoose");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    max: 255,
+  },
+  email: {
+    type: String,
+    required: true,
+    max: 20,
+  },
+  password: {
+    type: String,
+    required: true,
+    max: 255,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
