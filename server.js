b39e90df7e5edb1aca30de6fd659a3a9cfb9e595
Alhassan Ibrahim <alhassan.ibrahim@flexisaf.com>
2020-11-07 13:46:26 +0100

    Init repo

---
+++ server.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const app = require('./app');
+
+const port = process.env.PORT || 3000;
+
+http.createServer(app).listen(port, () => {
+    console.log(`server running on http://localhost:${port}`);
+});
